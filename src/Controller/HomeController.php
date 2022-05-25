5dcdfb5abbf1ee502c9227733fdc353466a5ace0
EmmanuelleAllain <e.allain13@gmail.com>
2022-05-25 21:00:45 +0200

    manage the rounds

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,38 +4,54 @@ namespace App\Controller;
 
 class HomeController extends AbstractController
 {
-    public function index(): string
+    public function index(): ?string
     {
         $array = ['red', 'yellow','orange', 'green', 'purple','blue'];
-        $adversaryArrayKeys = array_rand($array, 4);
-        $adversaryArray = [
-            $array[$adversaryArrayKeys[0]],
-            $array[$adversaryArrayKeys[1]],
-            $array[$adversaryArrayKeys[2]],
-            $array[$adversaryArrayKeys[3]],
-        ];
+
+        if (empty($_SESSION['adversary'])) {
+            $adversaryArrayKeys = array_rand($array, 4);
+            $adversaryArray = [
+                $array[$adversaryArrayKeys[0]],
+                $array[$adversaryArrayKeys[1]],
+                $array[$adversaryArrayKeys[2]],
+                $array[$adversaryArrayKeys[3]],
+            ];
+            $_SESSION['adversary'] = $adversaryArray;
+        }
+
+        if (empty($_SESSION['rounds'])) {
+            $_SESSION['rounds'] = [];
+        }
+
+        if (empty($_SESSION['results'])) {
+            $_SESSION['results'] = [];
+        }
+
         $playerArray = [];
-        $player = '';
-        $blackMatching = 0;
-        $whiteMatching = 0;
-
-
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $colors = array_map('trim', $_POST);
-            $playerArray = $colors;
-
-            $similarValuePosition = array_intersect_assoc($adversaryArray, $playerArray);
-            $blackMatching = count($similarValuePosition);
-            $similarValue = array_intersect($adversaryArray, $playerArray);
-            $whiteMatching = count($similarValue) - $blackMatching;
-            $player = implode(' ', $playerArray);
+            if (isset($_POST['restart'])) {
+                if ($_POST['restart'] == 'restartGame') {
+                    session_destroy();
+                    header('Location: /');
+                    return null;
+                }
+            }
+            if (isset($_POST['submit'])) {
+                $colors = array_map('trim', $_POST);
+                $playerArray = $colors;
+                array_push($_SESSION['rounds'], $playerArray);
+                $similarValuePosition = array_intersect_assoc($_SESSION['adversary'], $playerArray);
+                $blackMatching = count($similarValuePosition);
+                $similarValue = array_intersect($_SESSION['adversary'], $playerArray);
+                $whiteMatching = count($similarValue) - $blackMatching;
+                $results = [$blackMatching, $whiteMatching];
+                array_push($_SESSION['results'], $results);
+            }
         }
         return $this->twig->render(
             'Home/index.html.twig',
             [
-            'player' => $player,
-            'blackMatching' => $blackMatching,
-            'whiteMatching' => $whiteMatching,
+            'sessions' => $_SESSION,
             ]
         );
     }
